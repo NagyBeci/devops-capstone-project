12f98a9c0e68ddab1a4d64390c8f36a965e74ad1
{NagyBeci} <{frosty_reloaded2@yahoo.com}>
2024-01-17 14:04:26 -0500

    Error Handlers-handled

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -151,7 +151,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-        
+
 ########## UPDATE ACCOUNTS UNITTESTS ############
 
     def test_update_account(self):
@@ -177,3 +177,9 @@ class TestAccountService(TestCase):
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
+########## ERROR HANDLERS UNITTESTS ############
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
